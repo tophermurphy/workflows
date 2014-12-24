42e22971b144d2142c10500d121b0799f3c91bfa
tophermurphy <tophermurphy@gmail.com>
2014-12-23 19:25:37 -0500

    mini html

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,8 @@ var gulp = require('gulp'),
     compass = require('gulp-compass'),
     connect = require('gulp-connect'),
     gulpif = require('gulp-if'),
-    uglify = require('gulp-uglify')
+    uglify = require('gulp-uglify'),
+    miniHtml = require('gulp-minify-html')
 ;
 
 var env, 
@@ -72,7 +73,7 @@ gulp.task('watch' , function (){
     gulp.watch(coffeeSources, ['coffee']);
     gulp.watch(jsSources , ['js']);
     gulp.watch('components/sass/*.scss' , ['compass']);
-    gulp.watch(htmlSources , ['html']);
+    gulp.watch('builds/developlment/*.html' , ['html']);
     gulp.watch(jsonSources , ['json']);
 });
 
@@ -84,7 +85,9 @@ gulp.task('connect' , function(){
 });
 
 gulp.task('html' , function(){
-    gulp.src(htmlSources)
+    gulp.src('builds/developlment/*.html')
+        .pipe(gulpif( env === 'production', miniHtml()))
+        .pipe(gulpif( env === 'production', gulp.dest(outputDir)))
         .pipe(connect.reload())
 });
 
